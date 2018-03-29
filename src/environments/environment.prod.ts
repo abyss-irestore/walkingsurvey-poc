e88aff324d3414cd8b3719b6b494c1e4cb5e6271
abhishek-irestore <abhishek@irestoreapp.com>
2018-03-29 19:36:09 +0530

    improved CSS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAPIv9rmrDawQJuPh341D0Qhjl8LY9epT8",
+        authDomain: "walkingsurvey-poc.firebaseapp.com",
+        databaseURL: "https://walkingsurvey-poc.firebaseio.com",
+        projectId: "walkingsurvey-poc",
+        storageBucket: "walkingsurvey-poc.appspot.com",
+        messagingSenderId: "275015514403"
+    }
 };
